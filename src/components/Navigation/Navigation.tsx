900e2e31c4e906740289c81d3e06d93c0598921f
PakooGD <popkov-lelya89@mail.ru>
2023-10-04 11:52:53 +0300

    feat: typescript routing is done

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,32 +1,27 @@
 import { createStackNavigator } from '@react-navigation/stack';
-import {Player, Welcome, Auth, PlaylistScreen} from '../Screens/index'
+import { Player, Welcome, Auth, PlaylistScreen } from '../Screens/index';
 import { TabsNavigation } from './TabsNavigation';
 
+type MainStackParamList = {
+  Welcome: undefined;
+  Auth: undefined;
+  NavStack: undefined;
+  TabsNavStack: undefined;
+  Player: undefined;
+  PlaylistScreen: undefined;
+};
 
-const Stack = createStackNavigator(); // Создаем стэк постраничной навигации
+const RootStack = createStackNavigator<MainStackParamList>();
 
-
-export function MainStack () {
+export function MainStack() {
   return (
-    /* Задаём начальный роутинг и формируем стэк. Первая траница - Welcome, дальше есть возможность перейти либо в приложение, либо к авторизации 
-    (необходимо, чтобы переход к приложению был возможен только после успешной авторизации).*/
-    <Stack.Navigator initialRouteName="Welcome" screenOptions={{headerShown: false}}>   
-      <Stack.Screen name="Welcome" component={Welcome} />
-      <Stack.Screen name="Auth" component={Auth} />
-      <Stack.Screen name="NavStack" component={NavStack} />
-    </Stack.Navigator>
-  );
-}
-
-// Создаем стэк приложения, где начальный маршрут и страница - табы, которые в свою очередь подгружают Home и создают роутинг между страницами приложения.
-// После идут страницы, которые отображаются поверх табов и основных страниц. ??? Возможно стоит сделать их модальными окнами попросту
-function NavStack ({ navigation }) {
-  return (
-    <Stack.Navigator initialRouteName="TabsNavStack" screenOptions={{headerShown: false,}}>
-      <Stack.Screen name="TabsNavStack" component={TabsNavigation} />
-      <Stack.Screen name="Player" component={Player} options={StackOptions}/>
-      <Stack.Screen name="PlaylistScreen" component={PlaylistScreen} options={StackOptions}/>
-    </Stack.Navigator>
+    <RootStack.Navigator initialRouteName="Welcome" screenOptions={{ headerShown: false }}>
+      <RootStack.Screen name="Welcome" component={Welcome} />
+      <RootStack.Screen name="Auth" component={Auth} />
+      <RootStack.Screen name="TabsNavStack" component={TabsNavigation} />
+      <RootStack.Screen name="Player" component={Player} options={StackOptions} />
+      <RootStack.Screen name="PlaylistScreen" component={PlaylistScreen} options={StackOptions} />
+    </RootStack.Navigator>
   );
 }
 
